8a40c4abf92241c9fc3b7c6106437633b81db30b
Martin J Frigaard <mjfrigaard@pm.me>
2025-07-01 10:20:51 -0700

    Expand UI and server

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,10 @@
 #' App UI
 #' 
 #' Application UI for Rsixer.
+#'
+#' The UI provides buttons for the `Counter` demo as well as inputs for
+#' collecting text, numeric, and file values. A table, plot, and download
+#' button display the outputs generated from these inputs.
 #' 
 #' @export
 #' 
@@ -8,6 +12,12 @@ app_ui <- function() {
   fluidPage(
     actionButton("increment_btn", "Increment"),
     actionButton("reset_btn", "Reset"),
+    textInput("text", "Text"),
+    numericInput("number", "Number", value = 0),
+    fileInput("file", "Upload File"),
+    tableOutput("input_table"),
+    plotOutput("input_plot"),
+    downloadButton("download_data", "Download Data"),
     textOutput("counter_text")
   )
 }
